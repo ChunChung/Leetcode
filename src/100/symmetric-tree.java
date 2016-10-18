88168fbc43bdad293ca714703ed8b30976085025
chunchung <chunchung.yang@gmail.com>
2016-10-18 16:10:00 -0700

    Added code

--- src/100/symmetric-tree.java
+++ src/100/symmetric-tree.java
@@ -9,6 +9,33 @@
  */
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
-        
+        if (root == null) {
+            return true;
+        }      
+
+        Queue leftQueue = new ArrayList();
+        Queue rightQueue = new ArrayList();
+
+        leftQueue.offer(root.left);
+        rightQueue.offer(root.right);
+        return bfs(leftQueue, rightQueue);
+    }
+
+    private boolean bfs(Queue leftQueue, Queue rightQueue) {
+        if (leftQueue.size() == 0 && rightQueue.size() == 0) {
+            return true;
+        }
+
+        if (leftQueue.size() != rightQueue.size()) {
+            return false;
+        }
+
+        TreeNode nullNode = new TreeNode(-1);
+        int n = leftQueue.size();
+
+        while (n > 0) {
+            n--;
+        }
+        return bfs(leftQueue, rightQueue);
     }
 }
